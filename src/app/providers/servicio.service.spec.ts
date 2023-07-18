921b5760b3ce0bc9cd1539efccb67fa34845c21d
EZambranoFx <86860458+EZambranoFx@users.noreply.github.com>
2023-07-18 00:42:26 -0500

    test Service y Componente

--- src/app/providers/servicio.service.spec.ts
+++ src/app/providers/servicio.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ServicioService } from './servicio.service';
 
 describe('ServicioService', () => {
   let service: ServicioService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ServicioService]
+    });
     service = TestBed.inject(ServicioService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should make a GET request to the correct URL', () => {
+    const expectedURL = 'https://pokedex-7b07d-default-rtdb.firebaseio.com/collection.json';
+
+    service.getResponse().subscribe();
+
+    const httpRequest = httpMock.expectOne(expectedURL);
+    expect(httpRequest.request.method).toBe('GET');
+  });
+
+  it('should return the response data', () => {
+    const responseData = { /* Coloca aquí los datos de respuesta esperados */ };
+
+    service.getResponse().subscribe((data) => {
+      expect(data).toEqual(responseData);
+    });
+
+    const httpRequest = httpMock.expectOne('');
+    httpRequest.flush(responseData);
+  });
+
+
 });
--- src/app/services/services.component.spec.ts
+++ src/app/services/services.component.spec.ts
@@ -2,9 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ServicesComponent } from './services.component';
 
+import { By } from '@angular/platform-browser';
+
 describe('ServicesComponent', () => {
   let component: ServicesComponent;
   let fixture: ComponentFixture<ServicesComponent>;
+  let dataRows: HTMLElement[];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +16,33 @@ describe('ServicesComponent', () => {
     fixture = TestBed.createComponent(ServicesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    dataRows = fixture.debugElement.queryAll(By.css('tr'))
+    .map((debugElement) => debugElement.nativeElement);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('h2 element', () => {
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    const p = bannerElement.querySelector('h2')!;
+    expect(p.textContent).toEqual('banner works!');
+   });
+
+   it('should display the correct number of rows', () => {
+    // Restar 1 para excluir la fila de encabezado
+    expect(dataRows.length - 1).toBe(component.data.length);
+  });
+  it('should display the correct data', () => {
+    const dataValues = dataRows.slice(1) // Excluir la fila de encabezado
+      .map((row) => Array.from(row.children).map((cell) => cell.textContent?.trim()));
+  
+    const expectedDataValues = component.data.map((datum) => [
+      String(datum.id),
+      datum.name,
+      datum.num
+    ]);
+  
+    expect(dataValues).toEqual(expectedDataValues);
   });
 });

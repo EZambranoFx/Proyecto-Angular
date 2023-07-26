ab9d83e264b64f2ccbc4a486a2e79036ec64ca20
EZambranoFx <86860458+EZambranoFx@users.noreply.github.com>
2023-07-26 00:47:29 -0500

    cambios fat

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -11,8 +11,10 @@ import { ServicioService } from '../providers/servicio.service';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
+
   //Atributo con el tipo de dato de la interfaz
   public data: Interfaz[] = [];
+  public itemsToShow = 10; // Inicialmente se muestran 10 elementos
   //Inyección de dependencia del servicio
   constructor(private dataProvider: ServicioService) { }
   //Ejecución de la petición y suscripción de la respuesta
@@ -20,5 +22,8 @@ export class ServicesComponent {
     this.dataProvider.getResponse().subscribe((response) => {
       this.data = (response as Interfaz[]);
     })
+  }
+  showMore() {
+    this.itemsToShow += 10; // Carga 10 elementos adicionales cuando se hace clic en "Ver más"
   }
 }
